9ae069b50044592df95a2d325dac66d23b64ca1b
jjeng08 <jjeng08@users.noreply.github.com>
2018-12-01 11:40:38 -0500

    Got reactstrap tied up and a modal installed. Also started getting database info to the page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
-import * as $ from 'axios';
+import KudosModal from './components/Modal';
+import User from './components/User';
+import * as axios from 'axios';
+
+
 
 class App extends Component {
 
   state = {
+    userList: []
+  }
+
+  getUsers = () => {
+    axios.get('/api/users')
+    .then((result) => {
+      this.setState({userList: result.data});
+      console.log(this.state.userList)
+    })
+  }
+
+  componentDidMount() {
+    this.getUsers();
   }
 
   render() {
     return (
 
-      <div className="App">Test</div>
+      <div className="App">
+        {this.state.userList.map((user) => (
+          <User
+            key={user._id}
+            name={user.username}
+          />
+        ))}
+        <KudosModal />
+      </div>
     );
   }
 }
 
 export default App;
-
-// import Option from './Option';
-
-       /* <select onChange={this.handleDelete}>
-          {this.state.notesList.map((option) => (
-            <Option
-              value={option._id}
-              content={option.content}
-            />
-          ))}
-        </select> */
---
+++ client/src/components/User.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const User = (props) => (
+	<div>
+		{props.name}
+	</div>
+)
+
+export default User;
