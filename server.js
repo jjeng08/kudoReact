101d4a96ce0c3c85546c2db108c7ce7437775c94
jjeng08 <jjeng08@users.noreply.github.com>
2018-12-05 08:55:01 -0500

    Server set up for mLab.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const mongoose =require('mongoose');
 
-const mongoose = require("mongoose");
+const request = require('request');
+const cheerio = require('cheerio');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -19,6 +22,15 @@ require('./routes/api-routes')(app);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/kudoReact");
 
+const db=mongoose.connection;
+
+db.on('error', function(err) {
+  console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function() {
+  console.log('Mongooseconnection successful.');
+})
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
